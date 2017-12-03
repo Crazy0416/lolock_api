41237b05e1e84e1dc0c384a3cf33edabc2212d36
Min-ho Kim <minmin0416@naver.com>
2017-12-03 12:31:08 +0900

    Fix path3

--- lolock_api/routes/api.js
+++ lolock_api/routes/api.js
@@ -599,7 +599,7 @@ router.delete('/disposable-link/:linkId', function(req, res, next) {
 
 /* GET 받은 일회용 키로 접속했을때 open_url.html을 response 한다. */
 router.get('/disposable-link/:linkId', function(req, res, next) {
-    res.sendFile(__dirname + '/public/html/open_url.html');
+    res.sendFile(__dirname + '/../public/html/open_url.html');
 });
 
 
